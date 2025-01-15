f475329d0576bfc216be2d1db309a34ec93dde5a
Adam Dangoor <adamdangoor@gmail.com>
2025-01-15 19:06:45 +0000

    Fix pylint errors

--- tests/test_confluence_toc.py
+++ tests/test_confluence_toc.py
@@ -1,5 +1,5 @@
 """
-Tests for Sphinx extensions.
+Tests for the ``..confluence_toc::`` directive.
 """
 
 from collections.abc import Callable
@@ -10,196 +10,184 @@ import pytest
 from sphinx.testing.util import SphinxTestApp
 
 
-class TestConfluenceTOC:
-    """Tests for the ``..
-
-    confluence_toc::`` directive.
+@pytest.mark.sphinx("html")
+def test_confluence_toc(
+    tmp_path: Path,
+    make_app: Callable[..., SphinxTestApp],
+) -> None:
     """
+    The table of contents directive renders like a normal table of contents.
+    """
+    source_directory = tmp_path / "source"
+    source_directory.mkdir()
 
-    @pytest.mark.sphinx("html")
-    def test_confluence_toc(
-        self,
-        tmp_path: Path,
-        make_app: Callable[..., SphinxTestApp],
-    ) -> None:
-        """
-        The table of contents directive renders like a normal table of
-        contents.
-        """
-        source_directory = tmp_path / "source"
-        source_directory.mkdir()
+    conf_py = source_directory / "conf.py"
+    conf_py_content = dedent(
+        text="""\
+        extensions = [
+            "sphinxcontrib.confluencebuilder",
+            "sphinx_confluencebuilder_bridge",
+        ]
+        """,
+    )
+    conf_py.write_text(data=conf_py_content)
 
-        conf_py = source_directory / "conf.py"
-        conf_py_content = dedent(
-            text="""\
-            extensions = [
-                "sphinxcontrib.confluencebuilder",
-                "sphinx_confluencebuilder_bridge",
-            ]
+    source_file = source_directory / "index.rst"
+    index_rst_template = dedent(
+        text="""\
+            Heading A
+            =========
+
+            Heading A.A
+            -----------
+
+            Some text
+
+            Heading A.B
+            -----------
+
+            Some text
+
+            {toc}
+
+            Heading A.B.A
+            ~~~~~~~~~~~~~
+
+            Text
+
+            Heading B
+            =========
+
+            Other
             """,
-        )
-        conf_py.write_text(data=conf_py_content)
+    )
 
-        source_file = source_directory / "index.rst"
-        index_rst_template = dedent(
-            text="""\
-             Heading A
-             =========
-
-             Heading A.A
-             -----------
-
-             Some text
-
-             Heading A.B
-             -----------
-
-             Some text
-
-             {toc}
-
-             Heading A.B.A
-             ~~~~~~~~~~~~~
-
-             Text
-
-             Heading B
-             =========
-
-             Other
-             """,
-        )
-
-        confluencebuilder_directive_source = dedent(
-            text="""\
-             .. confluence_toc::
-             """,
-        )
-
-        docutils_directive_source = dedent(
-            text="""\
-             .. contents::
-                :class: this-will-duplicate-information-and-it-is-still-useful-here
-             """,  # noqa: E501
-        )
-
-        source_file.write_text(
-            data=index_rst_template.format(
-                toc=confluencebuilder_directive_source,
-            ),
-        )
-
-        app = make_app(srcdir=source_directory)
-        app.build()
-        assert not app.warning.getvalue()
-
-        confluencebuilder_directive_html = (
-            app.outdir / "index.html"
-        ).read_text()
-        app.cleanup()
-
-        source_file.write_text(
-            data=index_rst_template.format(toc=docutils_directive_source),
-        )
-        app = make_app(srcdir=source_directory)
-        app.build()
-        assert not app.warning.getvalue()
-
-        docutils_directive_html = (app.outdir / "index.html").read_text()
-
-        assert confluencebuilder_directive_html == docutils_directive_html
-
-    @pytest.mark.sphinx("html")
-    def test_max_level(
-        self,
-        tmp_path: Path,
-        make_app: Callable[..., SphinxTestApp],
-    ) -> None:
-        """
-        The table of contents directive renders like a normal table of
-        contents, even with a ``max-level``.
-        """
-        source_directory = tmp_path / "source"
-        source_directory.mkdir()
-
-        conf_py = source_directory / "conf.py"
-        conf_py_content = dedent(
-            text="""\
-            extensions = [
-                "sphinxcontrib.confluencebuilder",
-                "sphinx_confluencebuilder_bridge",
-            ]
+    confluencebuilder_directive_source = dedent(
+        text="""\
+            .. confluence_toc::
             """,
-        )
-        conf_py.write_text(data=conf_py_content)
+    )
 
-        source_file = source_directory / "index.rst"
-        index_rst_template = dedent(
-            text="""\
-             Heading A
-             =========
+    docutils_directive_source = dedent(
+        text="""\
+            .. contents::
+            :class: this-will-duplicate-information-and-it-is-still-useful-here
+            """,
+    )
 
-             Heading A.A
-             -----------
+    source_file.write_text(
+        data=index_rst_template.format(
+            toc=confluencebuilder_directive_source,
+        ),
+    )
 
-             Some text
+    app = make_app(srcdir=source_directory)
+    app.build()
+    assert not app.warning.getvalue()
 
-             Heading A.B
-             -----------
+    confluencebuilder_directive_html = (app.outdir / "index.html").read_text()
+    app.cleanup()
 
-             Some text
+    source_file.write_text(
+        data=index_rst_template.format(toc=docutils_directive_source),
+    )
+    app = make_app(srcdir=source_directory)
+    app.build()
+    assert not app.warning.getvalue()
 
-             {toc}
+    docutils_directive_html = (app.outdir / "index.html").read_text()
 
-             Heading A.B.A
-             ~~~~~~~~~~~~~
+    assert confluencebuilder_directive_html == docutils_directive_html
 
-             Text
 
-             Heading B
-             =========
+@pytest.mark.sphinx("html")
+def test_max_level(
+    tmp_path: Path,
+    make_app: Callable[..., SphinxTestApp],
+) -> None:
+    """
+    The table of contents directive renders like a normal table of contents,
+    even with a ``max-level``.
+    """
+    source_directory = tmp_path / "source"
+    source_directory.mkdir()
 
-             Other
-             """,
-        )
+    conf_py = source_directory / "conf.py"
+    conf_py_content = dedent(
+        text="""\
+        extensions = [
+            "sphinxcontrib.confluencebuilder",
+            "sphinx_confluencebuilder_bridge",
+        ]
+        """,
+    )
+    conf_py.write_text(data=conf_py_content)
 
-        confluencebuilder_directive_source = dedent(
-            text="""\
-             .. confluence_toc::
-                :max-level: 1
-             """,
-        )
+    source_file = source_directory / "index.rst"
+    index_rst_template = dedent(
+        text="""\
+            Heading A
+            =========
 
-        docutils_directive_source = dedent(
-            text="""\
-             .. contents::
-                :class: this-will-duplicate-information-and-it-is-still-useful-here
-                :depth: 2
-             """,  # noqa: E501
-        )
+            Heading A.A
+            -----------
 
-        source_file.write_text(
-            data=index_rst_template.format(
-                toc=confluencebuilder_directive_source,
-            ),
-        )
+            Some text
 
-        app = make_app(srcdir=source_directory)
-        app.build()
-        assert not app.warning.getvalue()
+            Heading A.B
+            -----------
 
-        confluencebuilder_directive_html = (
-            app.outdir / "index.html"
-        ).read_text()
-        app.cleanup()
+            Some text
 
-        source_file.write_text(
-            data=index_rst_template.format(toc=docutils_directive_source),
-        )
-        app = make_app(srcdir=source_directory)
-        app.build()
-        assert not app.warning.getvalue()
+            {toc}
 
-        docutils_directive_html = (app.outdir / "index.html").read_text()
+            Heading A.B.A
+            ~~~~~~~~~~~~~
 
-        assert confluencebuilder_directive_html == docutils_directive_html
+            Text
+
+            Heading B
+            =========
+
+            Other
+            """,
+    )
+
+    confluencebuilder_directive_source = dedent(
+        text="""\
+            .. confluence_toc::
+            :max-level: 1
+            """,
+    )
+
+    docutils_directive_source = dedent(
+        text="""\
+            .. contents::
+            :class: this-will-duplicate-information-and-it-is-still-useful-here
+            :depth: 2
+            """,
+    )
+
+    source_file.write_text(
+        data=index_rst_template.format(
+            toc=confluencebuilder_directive_source,
+        ),
+    )
+
+    app = make_app(srcdir=source_directory)
+    app.build()
+    assert not app.warning.getvalue()
+
+    confluencebuilder_directive_html = (app.outdir / "index.html").read_text()
+    app.cleanup()
+
+    source_file.write_text(
+        data=index_rst_template.format(toc=docutils_directive_source),
+    )
+    app = make_app(srcdir=source_directory)
+    app.build()
+    assert not app.warning.getvalue()
+
+    docutils_directive_html = (app.outdir / "index.html").read_text()
+
+    assert confluencebuilder_directive_html == docutils_directive_html

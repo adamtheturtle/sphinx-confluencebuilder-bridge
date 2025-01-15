f1da79118df870cb23d06e0d40d773bbaf017b5a
Adam Dangoor <adamdangoor@gmail.com>
2025-01-15 22:51:41 +0000

    Passing test for server URL not given

--- tests/test_confluence_mention.py
+++ tests/test_confluence_mention.py
@@ -89,7 +89,8 @@ def test_mentioned_does_not_exist(
     make_app: Callable[..., SphinxTestApp],
 ) -> None:
     """
-    The ``:confluence_mention:`` role renders like a link to a user profile.
+    An error is raised if the ``confluence_bridge_users`` configuration value
+    does not contain the given user.
     """
     source_directory = tmp_path / "source"
     source_directory.mkdir()
@@ -147,7 +148,8 @@ def test_users_not_given(
     make_app: Callable[..., SphinxTestApp],
 ) -> None:
     """
-    The ``:confluence_mention:`` role renders like a link to a user profile.
+    An error is raised if the ``confluence_bridge_users`` configuration value
+    is not given.
     """
     source_directory = tmp_path / "source"
     source_directory.mkdir()
@@ -188,6 +190,63 @@ def test_users_not_given(
     expected_regex = (
         "The user 'eloise.red' is not in the "
         "'confluence_bridge_users' configuration value."
+    )
+    with pytest.raises(
+        expected_exception=ExtensionError,
+        match=expected_regex,
+    ):
+        app.build()
+
+
+@pytest.mark.sphinx("html")
+def test_server_url_not_given(
+    tmp_path: Path,
+    make_app: Callable[..., SphinxTestApp],
+) -> None:
+    """
+    The ``:confluence_mention:`` role renders like a link to a user profile.
+    """
+    source_directory = tmp_path / "source"
+    source_directory.mkdir()
+
+    conf_py = source_directory / "conf.py"
+    conf_py_content = dedent(
+        text="""\
+        extensions = [
+            "sphinxcontrib.confluencebuilder",
+            "sphinx_confluencebuilder_bridge",
+        ]
+
+        confluence_bridge_users = {
+            "eloise.red": "1234a",
+        }
+        """,
+    )
+    conf_py.write_text(data=conf_py_content)
+
+    source_file = source_directory / "index.rst"
+    index_rst_template = dedent(
+        text="""\
+            {mention}
+            """,
+    )
+
+    confluencebuilder_directive_source = dedent(
+        text="""\
+            :confluence_mention:`eloise.red`
+            """,
+    )
+
+    source_file.write_text(
+        data=index_rst_template.format(
+            mention=confluencebuilder_directive_source,
+        ),
+    )
+
+    app = make_app(srcdir=source_directory)
+    expected_regex = (
+        "The 'confluence_server_url' configuration value is required for the "
+        "'confluence_mention' role."
     )
     with pytest.raises(
         expected_exception=ExtensionError,

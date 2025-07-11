b626cf9b0b172488117079c0b5433675eb990765
Adam Dangoor <adamdangoor@gmail.com>
2025-07-11 16:13:28 +0100

    Expand test

--- tests/test_confluence_viewpdf.py
+++ tests/test_confluence_viewpdf.py
@@ -16,6 +16,7 @@ def test_confluence_viewpdf(
     The ``..confluence_viewpdf::`` directive renders like a normal PDF link.
     """
     source_directory = tmp_path / "source"
+    build_directory = tmp_path / "build"
     source_directory.mkdir()
     (source_directory / "conf.py").touch()
     pdf_path = Path(__file__).parent / "data" / "example.pdf"
@@ -52,6 +53,7 @@ def test_confluence_viewpdf(
 
     app = make_app(
         srcdir=source_directory,
+        builddir=build_directory,
         confoverrides={
             "extensions": [
                 "sphinxcontrib.confluencebuilder",
@@ -62,6 +64,14 @@ def test_confluence_viewpdf(
     app.build()
     assert app.statuscode == 0
     assert not app.warning.getvalue()
+    # Check that we do not pollute the source directory with generated files.
+    assert set(source_directory.glob(pattern="**")) == {
+        source_directory,
+        source_directory / "index.rst",
+        source_directory / "conf.py",
+        source_directory / "example.pdf",
+        source_directory / "example.png",
+    }
 
     confluencebuilder_directive_html = (app.outdir / "index.html").read_text()
     app.cleanup()
